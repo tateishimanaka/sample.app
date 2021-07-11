3169e5fca691a6a937f93af3bec444b80ac95bf0
TeiahiManaka <monakanomannaka.721@gmail.com>
2021-07-11 12:42:12 +0000

    [Finish2-6]

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -5,9 +5,7 @@ class TodolistsController < ApplicationController
   
   def create
     list = List.new(list_params)
-    
     list.save
-    
     redirect_to todolist_path(list.id)
   end
   
@@ -23,10 +21,18 @@ class TodolistsController < ApplicationController
     @list = List.find(params[:id])
   end
   
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to todolist_path(list.id)
+  end
+  
   private
   
   def list_params
       params.require(:list).permit(:title, :body)
   end
+ 
+  
 end
   
