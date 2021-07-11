c65febeb51dfb31eac530722a0a0673741a57132
TeiahiManaka <monakanomannaka.721@gmail.com>
2021-07-11 05:19:26 +0000

    [Finish2-5]

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -8,7 +8,15 @@ class TodolistsController < ApplicationController
     
     list.save
     
-    redirect_to '/top'
+    redirect_to todolist_path(list.id)
+  end
+  
+  def index
+    @lists = List.all
+  end
+  
+  def show
+    @list = List.find(params[:id])
   end
   
   private
