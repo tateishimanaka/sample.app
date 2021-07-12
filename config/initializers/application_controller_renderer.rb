a552bc09a35e20caf58550ad7c959d7da958a033
TeiahiManaka <monakanomannaka.721@gmail.com>
2021-07-12 13:26:52 +0000

    [Finish2-7]

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'd1d6ec383a317e7ee5674b8b88d0a4d401aa3783ec592834b5f718b9782a2fc91e87e43076c93fca5c921d980b14b51141ad65ea8477650c57fb2e9d7f8d7c79'
