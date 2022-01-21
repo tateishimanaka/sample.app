4383be3cc2bf8373aede2ca7cedcb05e0280f97c
tateishimanaka <monakanomannaka.721@gmail.com>
2022-01-21 11:54:26 +0000

    system

--- spec/models/list_spec.rb
+++ spec/models/list_spec.rb
@@ -2,10 +2,9 @@
 
 require 'rails_helper'
 
-Rspec.describe List, "モデルに関するテスト", type: :model do
-  describe '実際にテストしてみる' do
-    it "有効な投稿内容の場合は保存されるか" do
-      expect(FactoryBot.build(:list)).to be_valid
-    end
+
+describe 'モデルのテスト' do
+  it "有効な投稿内容の場合は保存されるか" do
+    expect(FactoryBot.build(:list)).to be_valid
   end
 end
