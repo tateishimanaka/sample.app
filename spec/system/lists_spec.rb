88c6e31976bbcc52efa334831ca26fc49d0ba050
tateishimanaka <monakanomannaka.721@gmail.com>
2022-01-22 15:25:12 +0000

    create-test

--- spec/system/lists_spec.rb
+++ spec/system/lists_spec.rb
@@ -7,10 +7,39 @@ describe '投稿のテスト' do
   describe 'トップ画面(top_path)のテスト' do
     before do
       visit top_path
+    end
+    context '表示の確認' do
+      it 'トップ画面(top_path)に「ここはTopページです」が表示されているか' do
+        expect(page).to have_content 'ここはTopページです'
+      end
+      it 'top_pathが"/top"であるか' do
+        expect(current_path).to eq('/top')
+      end
+    end
   end
-  context '表示の確認' do
+  describe '投稿画面のテスト'
+    before do
+      visit todolists_new_path 
+    end
+    context '表示の確認' do
+      it 'todolists_new_pathが"/todolists/new"であるか' do
+        expect(current_path).to eq('todolists/new') 
+      end
+      it '投稿ボタンが表示されているか' do
+        expect(page).to have_button '投稿'
+      end
+    end
+    context '投稿処理のテスト' do
+      it '投稿後のリダイレクト先は正しいか' do
+        
+      end
+    end
+  end
+  describe '一覧画面のテスト' do
+    before do
+      visit todolists_path
+    end
+    
     
   end
-  end
-  
 end
