825189e169aa38429ca813eeff275fb3b3e22b0d
tateishimanaka <monakanomannaka.721@gmail.com>
2022-01-22 08:47:43 +0000

    rails db:migrate

--- spec/system/lists_spec.rb
+++ spec/system/lists_spec.rb
@@ -3,5 +3,14 @@
 require 'rails_helper'
 
 describe '投稿のテスト' do
+  let!(:list) { create(:list,title:'hoge',body:'body') }
+  describe 'トップ画面(top_path)のテスト' do
+    before do
+      visit top_path
+  end
+  context '表示の確認' do
+    
+  end
+  end
   
 end
