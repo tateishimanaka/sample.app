67a23e30274febc230552af67f68f8d6d13c122a
tateishimanaka <monakanomannaka.721@gmail.com>
2022-01-23 08:40:28 +0000

    show-test

--- spec/system/lists_spec.rb
+++ spec/system/lists_spec.rb
@@ -17,7 +17,7 @@ describe '投稿のテスト' do
       end
     end
   end
-  describe '投稿画面のテスト'
+  describe '投稿画面のテスト' do
     before do
       visit todolists_new_path 
     end
@@ -31,15 +31,30 @@ describe '投稿のテスト' do
     end
     context '投稿処理のテスト' do
       it '投稿後のリダイレクト先は正しいか' do
-        
+        expect(current_path).to eq '/todolists/' + List.last.id.to_s
       end
     end
   end
+  
   describe '一覧画面のテスト' do
     before do
       visit todolists_path
     end
-    
-    
+    context '一覧の表示とリンクの確認' do
+      it  '一覧表示画面に投稿されたもの表示されているか' do
+        expect(page).to have_link list.title, href: todolist_path(list)
+        expect(page).to have_content list.body
+      end
+    end
+  end
+  describe '詳細画面のテスト' do
+    before do
+      visit todolist_path(list)
+    end
+    context '表示のテスト' do
+      it '削除リンクが存在しているか' do
+        expect(page).to have_link 
+      end
+    end
   end
 end
